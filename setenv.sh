0b8b9810bd7314d36eb7f4ab1e97d014aa8581c1
Diego Rivera <diego.rivera@armedia.com>
2025-11-05 14:03:41 -0600

    Remove LD_LIBRARY_PATH disclosure to the logs

--- setenv.sh
+++ setenv.sh
@@ -20,5 +20,4 @@ if "install-tomcat-native-module" ; then
 	#
 	echo "The Tomcat Native module was installed"
 	export LD_LIBRARY_PATH="${CATALINA_BASE}/lib:${LD_LIBRARY_PATH}"
-	echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH@Q}"
 fi
